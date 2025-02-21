d7c8652ed7b508651b8fa37254fb74eb547fa15b
AllanMaloi <maloisaruni@gmail.com>
2025-02-21 19:54:14 +0300

    Updated views to include correct template paths and ensured proper template directory structure

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Book, Library
 
+# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-
-from django.views.generic.detail import DetailView
-from .models import Library
-
+# Class-based view to display details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
