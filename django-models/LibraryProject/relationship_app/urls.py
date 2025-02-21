afc998b8da9807a96356377d2b1f5574027da9a9
AllanMaloi <maloisaruni@gmail.com>
2025-02-21 22:30:36 +0300

    Added URL pattern for register view in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import register
+from . import views  # Import views from the current directory
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
