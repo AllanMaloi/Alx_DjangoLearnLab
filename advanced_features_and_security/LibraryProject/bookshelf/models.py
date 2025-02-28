96c917344b76110fa55da4a5c9598b1876a6aabb
AllanMaloi <maloisaruni@gmail.com>
2025-02-28 15:41:07 +0300

    Moved CustomUser model to users app and cleaned up bookshelf models

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,12 +1,6 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
-from django.db import models
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
-
-    def __str__(self):
-        return self.title
+class SomeModel(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # Other fields...
