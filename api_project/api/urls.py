f3503c46756e2c9b4d27832bc711f8e45fe83454
AllanMaloi <maloisaruni@gmail.com>
2025-03-07 15:13:26 +0300

    Implemented authentication and permissions in DRF

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet, BookList
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -12,4 +13,7 @@ urlpatterns = [
 
     # Include the router URLs for BookViewSet (all CRUD operations)
     path('', include(router.urls)),  # This includes all routes registered with the router
+
+
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,6 @@
 # api/views.py
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
@@ -12,3 +13,8 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Apply permissions here
