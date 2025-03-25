169d91c0bb7a9b98e815512c818ab677de7783c6
AllanMaloi <maloisaruni@gmail.com>
2025-03-25 23:20:22 +0300

    made changes to views.py in accounts

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,6 +5,8 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework import status, permissions, generics
 from django.contrib.auth import get_user_model
 from accounts.serializers import CustomUserSerializer
+from .serializers import UserSerializer
+
 
 User = get_user_model()
 
@@ -25,18 +27,20 @@ class CustomAuthToken(ObtainAuthToken):
         token = response.data['token']
         return Response({'token': token}, status=status.HTTP_200_OK)
     
-class FollowUserView(APIView):
+class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
+    queryset = User.objects.all()
 
     def post(self, request, user_id):
-        user_to_follow = User.objects.get(id=user_id)
+        user_to_follow = self.get_queryset().get(id=user_id)
         request.user.following.add(user_to_follow)
         return Response({"message": f"You are now following {user_to_follow.username}"}, status=status.HTTP_200_OK)
 
-class UnfollowUserView(APIView):
+class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
+    queryset = User.objects.all()
 
     def post(self, request, user_id):
-        user_to_unfollow = User.objects.get(id=user_id)
+        user_to_unfollow = self.get_queryset().get(id=user_id)
         request.user.following.remove(user_to_unfollow)
         return Response({"message": f"You have unfollowed {user_to_unfollow.username}"}, status=status.HTTP_200_OK)
