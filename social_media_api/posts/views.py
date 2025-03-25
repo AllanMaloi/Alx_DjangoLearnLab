6c9662c61b5e4cb96a807efacd3ad9c13f7f767d
AllanMaloi <maloisaruni@gmail.com>
2025-03-25 22:49:55 +0300

    fixed an error

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, filters 
+from rest_framework import viewsets, permissions, filters
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
@@ -15,16 +15,13 @@ class PostViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'content']
 
-
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')  # ✅ Fix for error
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
-
-    def get_queryset(self):
-        return Comment.objects.filter(post_id=self.kwargs['post_pk'])
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
